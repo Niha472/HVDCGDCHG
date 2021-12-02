014d1e6e6b79d52f25e174e65039cc6b18570fc4
Niha472 <72551894+Niha472@users.noreply.github.com>
2021-12-02 16:00:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+camera=document.getElementById("camera")
+Webcam.attach('#camera')
+function clickme()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("action").innerHTML='<img id="captured_img" src="'+data_uri+'"/>';
+    });
+}
+console.log(ml5.version)
